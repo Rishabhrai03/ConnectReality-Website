a8465d6cf28946d55481ab1d8b9008cf1848840d
Rishabh Rai <rishabh.rai@voloforce.com>
2022-07-04 22:17:48 +0530

    first commit

---
+++ www.connectreality.net/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        margin: 10,
+        autoHeight: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: false,
+        loop: true
+    });
+});
+
